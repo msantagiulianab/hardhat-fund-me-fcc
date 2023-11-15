fd3ef0a0cbb52dbe52b6b9b811c29a36aef1bbc0
michele <michelesantagiuliana@hotmail.it>
2023-11-15 19:21:39 +0000

    initial commit

---
+++ helper-hardhat-config.js
@@ -0,0 +1,22 @@
+const networkConfig = {
+    11155111: {
+        name: "sepolia",
+        ethUsdPriceFeed: "0x694AA1769357215DE4FAC081bf1f309aDC325306",
+    },
+    137: {
+        name: "polygon",
+        ethUsdPriceFeed: "0xf9680d99d6c9589e2a93a78a04a279e509205945",
+    },
+    // 31337
+}
+
+const developmentChains = ["hardhat", "localhost"]
+const DECIMALS = 8
+const INITIAL_ANSWER = 200000000000
+
+module.exports = {
+    networkConfig,
+    developmentChains,
+    DECIMALS,
+    INITIAL_ANSWER,
+}
